d69fcdb9d1f6a1f5c340a048858bd32e17e3adc7
c9long <109810526+c9long@users.noreply.github.com>
2023-03-30 10:47:20 -0400

    fixed textdisplay.cc

--- textdisplay.cc
+++ textdisplay.cc
@@ -1,14 +1,16 @@
 #include "textdisplay.h"
 
 Textdisplay::Textdisplay() {
-    for (int i = 0; i < )
+    for (int i = 0; i < theBoard.size(); ++i) {
+
+    }
 }
 
 std::ostream &operator<<(std::ostream &out, const Textdisplay &td)
 {
-    for (int i = 0; i < theBoard.size(); ++i)
+    for (int i = 0; i < td.theBoard.size(); ++i)
     {
-        for (int j = 0; j < theBoard.at(i).size(); ++j)
+        for (int j = 0; j < td.theBoard.at(i).size(); ++j)
         {
             out << td.theBoard[i][j];
         }

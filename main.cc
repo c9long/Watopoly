4b7b06530e46e1fdbcb6d79e9211877aa4dad26b
c9long <109810526+c9long@users.noreply.github.com>
2023-04-02 12:02:18 -0400

    updating main

--- main.cc
+++ main.cc
@@ -4,16 +4,23 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     int numPlayers = 0;
-    if (argv[1] == "-load") {
-        ifstream ifs{argv[2]};
-        string s;
-        while(getline(ifs, s, ' ')) {
-            cout << s << endl;
+    if (argc > 1)
+    {
+        string arg = argv[1];
+        if (arg == "-load")
+        {
+            cout << "success" << endl;
+            ifstream ifs{argv[2]};
+            string s;
+            while (getline(ifs, s, ' '))
+            {
+                cout << s << endl;
+            }
         }
     }
-    
-    cout << "How many players would you like to enter into your new game?" << endl;
-    return 10;
-}
+
+        cout << "How many players would you like to enter into your new game?" << endl;
+    }

54aa2bc139f889fefdc1cebb84bd3d2215f6f2c1
c9long <109810526+c9long@users.noreply.github.com>
2023-04-02 11:50:35 -0400

    updating main.cc

--- main.cc
+++ main.cc
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main() {
-    std::cout << "10" << std::endl;
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    int numPlayers = 0;
+    if (argv[1] == "-load") {
+        ifstream ifs{argv[2]};
+        string s;
+        while(getline(ifs, s, ' ')) {
+            cout << s << endl;
+        }
+    }
+    
+    cout << "How many players would you like to enter into your new game?" << endl;
     return 10;
 }
